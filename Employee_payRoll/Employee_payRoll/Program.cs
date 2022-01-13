1af060a6bd4e2f0c6f44671ff0e958287b12a2e8
Harsh Singh <kmrharsh50@gmail.com>
2022-01-13 19:46:52 +0530

    [Harsh]Added:Welcome Message Employee_payRoll_ADO.Net feature implemented

---
+++ Employee_payRoll/Employee_payRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_payRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wecome to Employee PayRoll Problem using Ado.Net!");
+        }
+    }
+}
